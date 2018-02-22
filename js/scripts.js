66b6c21ea20797fbb4b2322d7b3eadf06e391c25
zecollokaris <collo.kariss@gmail.com>
2018-02-22 18:44:15 +0300

    Add jquery-proj

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function() {
+  jQuery("h1").click(function() {
+    alert("This is a header.");
+     alert("I told you, THIS IS A HEADER!");
+  });
+
+  jQuery("p").click(function() {
+    alert("This is a paragraph.");
+  });
+
+  jQuery("img").click(function() {
+    alert("This is an image.");
+
+  });
+});
